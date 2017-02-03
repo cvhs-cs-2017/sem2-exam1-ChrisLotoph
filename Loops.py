5469887b12ef8862a0cf45c85e4a40ffc7caacf4
ChrisLotoph <lech111399@cvh.rusd.us>
2017-02-03 10:43:55 -0800

    confused

--- Loops.py
+++ Loops.py
@@ -1,12 +1,14 @@
 """Write a program that will add 5 and loop until it reaches a number GREATER
 than 100.  It should then spit out the result AND tell the user how many times
 it had to add 5 (if any)"""
-
-
-
-
-
+n = int(input('Enter a number '))
+def addfive(n):
+    while n >= 100:
+print (addfive)
 
 """Write a program that will prompt the user for an input value (n) and double
 it IF is an ODD number, triple it if is an EVEN number and do nothing if it is
 anything else (like a decimal or a string)"""
+n = int(input('Enter a number '))
+def double(n):
+    n = n
